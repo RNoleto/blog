10bbb20fa3afbf29d42566a83ce0832d1649c7e1
Ramon Ribeiro Noleto <95661913249@itajai.local>
2024-03-25 13:29:40 -0300

    ajustes composer aula 49

--- index.php
+++ index.php
@@ -1,12 +1,8 @@
 <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous"> -->
 <?php
 
-require_once 'sistema/configuracao.php';
-include_once 'sistema/Nucleo/helpers.php';
-include './sistema/Nucleo/Mensagem.php';
-include './sistema/Nucleo/Controlador.php';
+require 'vendor/autoload.php';
 
-use sistema\Nucleo\Controlador;
+echo SITE_NOME;
 
-$controlador = new Controlador();
-echo '<hr>';
+echo sistema\Nucleo\Helpers::saudacao();
