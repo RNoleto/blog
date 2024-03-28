441b2ae4d9dad45d7a14a1ab194073cf34e31778
Ramon Ribeiro Noleto <95661913249@itajai.local>
2024-03-28 09:57:18 -0300

    conexão do PDO finalizada

---
+++ sistema/Nucleo/Conexao.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace sistema\Nucleo;
+
+use PDO;
+use PDOException;
+
+/**
+ * Classe Conexao
+ * 
+ * @author Ramon Noleto
+ */
+class Conexao
+{
+    private static $instancia;
+
+    public static function getInstancia():PDO
+    {
+        if(empty(self::$instancia)){
+            try {
+                self::$instancia = new PDO('mysql:host='.DB_HOST.';port='.DB_PORTA.';dbname='.DB_NOME, DB_USUARIO, DB_SENHA, [
+                    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
+                    PDO::ATTR_CASE => PDO::CASE_NATURAL
+                ]); 
+            } catch (PDOException $ex) {
+                die("Erro de conexão:: ".$ex->getMessage());
+            }
+            return self::$instancia;
+        }
+    }
+
+    protected function __construct()
+    {
+        
+    }
+
+    private function __clone(): void
+    {
+        
+    }
+}
--- sistema/configuracao.php
+++ sistema/configuracao.php
@@ -4,6 +4,13 @@
 //Define o fuso horario
 date_default_timezone_set('America/Sao_Paulo');
 
+//Configuração banco de dados do blog
+define('DB_HOST', 'localhost');
+define('DB_PORTA', '3306');
+define('DB_NOME', 'blog');
+define('DB_USUARIO', 'root');
+define('DB_SENHA', '');
+
 //informações do site
 define('SITE_NOME', 'UnSet');
 define('SITE_DESCRICAO', 'UnSet - Tecnologia em Sistemas');
